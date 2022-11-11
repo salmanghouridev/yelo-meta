474042befaea8a80664f686cfb4c3356ad144975
Salman Ghouri <64996174+Salmanghouri@users.noreply.github.com>
2022-11-11 23:29:53 +0500

    v1.0

--- pages/index.js
+++ pages/index.js
@@ -8,8 +8,26 @@ export default function Home() {
   return (
    <>
    
-   <Navbar/>
+   {/* <Navbar/>
    <Banner/>
+    */}
+    {/* <Navbar/> */}
+    <section className="bg-[#0a0f0c] text-white">
+  <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
+    <div className="mx-auto max-w-xl text-center">
+      <h1 className="text-3xl font-extrabold sm:text-5xl">
+      Comming Soon
+       
+      </h1>
+      <p className="mt-4 sm:text-xl sm:leading-relaxed">
+        The Biggest Online Developer OpenSource Community Ever...
+      </p>
+    
+    </div>
+  </div>
+</section>
+
+
    </>
   )
 }
